234c423917cfc798ec299f1476b10a0df91698ad
Hmoovzong Vang <hmoovzongvang@gmail.com>
2024-01-26 18:20:44 -0600

    Created button to go to AddMovie page. Linked all posters to a details page to view details

---
+++ src/components/AddMovie/AddMovie.jsx
@@ -0,0 +1,9 @@
+export const AddMovie = () => {
+    console.log("Here in AddMovie");
+    return (
+        <div>
+            <h1>Add Movie</h1>
+        </div>
+    );
+}
+
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,20 +1,25 @@
-import { Route, HashRouter as Router } from 'react-router-dom';
-import MovieList from '../MovieList/MovieList';
-import './App.css';
+import { Route, HashRouter as Router } from "react-router-dom";
+import MovieList from "../MovieList/MovieList";
+import "./App.css";
+import { DetailsPage } from "../DetailsPage/DetailsPage";
+import { AddMovie } from "../AddMovie/AddMovie";
 
 function App() {
   return (
     <div className="App">
       <h1>The Movies Saga!</h1>
-      <Router>        
+      <Router>
         <Route path="/" exact>
           <MovieList />
         </Route>
-        
-        {/* Details page */}
 
-        {/* Add Movie page */}
-        
+        <Route path="/DetailsPage">
+          <DetailsPage />
+        </Route>
+
+        <Route path="/AddMovie">
+          <AddMovie />
+        </Route>
       </Router>
     </div>
   );
